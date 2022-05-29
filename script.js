e88e824c41966742dca60922156c43488dd58d76
Sakina-IssaNoha <sakinnanoha@gmail.com>
2022-05-29 19:43:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const messageInput = document.getElementById("message-input");
+
+function getmessage(){
+    document.getElementById("message-output").innerHTML = messageInput.value;messageInput.value = "";
+}
